9f522093ff1708ad1c4bedc79a6c375607892219
Pranjay kumar <110048711+pranjaykumar926@users.noreply.github.com>
2026-03-23 22:43:07 +0530

    Update stl_store.cpp

--- 09_stl/stl_store.cpp
+++ 09_stl/stl_store.cpp
@@ -9,7 +9,6 @@
 #include <unordered_set>
 #include <unordered_map>
 
-
 using namespace std;
 
 struct Product {
@@ -20,34 +19,34 @@ struct Product {
 
 struct Order {
     int orderID;
-    int ProductID;
+    int productID;
     int quantity;
     string customerID;
     time_t orderDate;
 };
 
-int main(){
+int main() {
     vector<Product> products = {
         {101, "Laptop", "Electronics"},
         {102, "SmartPhone", "Electronics"},
-        {103, "Coffe Maker", "Kitchen"},
+        {103, "Coffee Maker", "Kitchen"},
         {104, "Blender", "Kitchen"},
         {105, "Desk lamp", "Home"},
     };
 
     deque<string> recentCustomers = {"C001", "C002", "C003"};
 
-    recentCustomers.push_back("C004");
-    recentCustomers.push_front("C005");
+    recentCustomers.emplace_back("C004");
+    recentCustomers.emplace_front("C005");
 
     list<Order> orderHistory;
 
-    orderHistory.push_back({1, 101, 1, "C001", time(0)});
-    orderHistory.push_back({2, 102, 2, "C002", time(0)});
-    orderHistory.push_back({3, 103, 1, "C003", time(0)});
+    orderHistory.emplace_back(Order{1, 101, 1, "C001", time(0)});
+    orderHistory.emplace_back(Order{2, 102, 2, "C002", time(0)});
+    orderHistory.emplace_back(Order{3, 103, 1, "C003", time(0)});
 
     set<string> categories;
-    for(const auto &product: products){
+    for (const auto &product: products) {
         categories.insert(product.category);
     }
 
@@ -60,24 +59,40 @@ int main(){
     };
 
     multimap<string, Order> customerOrders;
-    for(const auto &order: orderHistory){
+    for (const auto &order: orderHistory) {
         customerOrders.insert({order.customerID, order});
     }
 
     unordered_map<string, string> customerData = {
         {"C001", "Alice"},
-        {"C002", "hitesh"},
+        {"C002", "Hitesh"},
         {"C003", "Vidya"},
-        {"C004", "max"},
-        {"C005", "harry"},
+        {"C004", "Max"},
+        {"C005", "Harry"},
     };
 
     unordered_set<int> uniqueProductIDs;
 
-    for(const auto &product: products){
+    for (const auto &product: products) {
         uniqueProductIDs.insert(product.productID);
     }
 
+    cout << "--- System Initialized Successfully ---\n" << endl;
+
+    string searchCustomer = "C002";
+    auto range = customerOrders.equal_range(searchCustomer);
+
+    cout << "Orders for " << customerData[searchCustomer] << ":" << endl;
+    for (auto it = range.first; it != range.second; ++it) {
+        cout << "- Order ID: " << it->second.orderID 
+             << " | Product ID: " << it->second.productID 
+             << " | Qty: " << it->second.quantity << endl;
+    }
+
+    cout << "\nUnique Product Categories (" << categories.size() << "):" << endl;
+    for(const auto &category : categories) {
+        cout << "- " << category << endl;
+    }
 
     return 0;
 }
